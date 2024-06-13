13d65221d5e0c50da9aaba83d6d4963adbd1f83b
Limak1402 <limak1402@gmail.com>
2024-06-13 16:42:54 +0200

    WIP

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import videoFile from '../Assets/red-panda.mp4'
+
+const Introduction = () => {
+  return (
+    <div>
+      <video width="100%" controls autoPlay muted>
+        <source src={videoFile} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <p className='description'>
+      Panda ruda, znana także jako panda czerwona, to uroczy 
+      mieszkaniec południowo-zachodnich Chin. Charakteryzuje się 
+      jasnoczerwonym futrem z białymi plamami na twarzy, uszach i ciele. 
+      Jest gatunkiem zagrożonym wyginięciem głównie z powodu utraty 
+      naturalnego środowiska oraz kłusownictwa. Pandki rude są 
+      roślinożercami, żywiącymi się głównie bambusem, który stanowi 
+      około 99% ich diety. Są symbolem ochrony przyrody i przypominają nam,
+      jak ważne jest zachowanie różnorodności biologicznej naszej planety.
+      </p>
+    </div>
+  )
+}
+
+export default Introduction
